8475719bc6c68651c9e63154040699bd210c02cb
Jack Henry <jack@jackerickson.net>
2021-03-07 12:51:28 -0600

    Added parseoption and output command line options.

--- app/src/main/java/github/jackhenry/CLI.java
+++ app/src/main/java/github/jackhenry/CLI.java
@@ -3,6 +3,7 @@
  */
 package github.jackhenry;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.util.concurrent.Callable;
 
@@ -16,9 +17,29 @@ public class CLI implements Callable<Integer> {
     @Option(names = { "-c", "--class" })
     private String classFilePath;
 
+    enum ParseOption {
+        SKIP_CODE, SKIP_DEBUG, SKIP_FRAMES, EXPAND_FRAMES
+    }
+
+    @Option(names = { "-p", "--parseoption" }, arity = "0..1", description = { "(Optional)",
+            "Parse Options: ${COMPLETION-CANDIDATES}", "Default: EXPAND_FRAMES" })
+    private ParseOption parseOption = ParseOption.EXPAND_FRAMES;
+
+    @Option(names = { "-o",
+            "--output" }, arity = "0..1", description = "Optional file path to write asmifier output into")
+    private String outputFilePath = null;
+
     @Override
     public Integer call() throws Exception {
-        PrintWriter printWriter = new PrintWriter(System.out);
+        PrintWriter printWriter;
+
+        // Write output to standard out if --output option is not passed
+        if (outputFilePath == null) {
+            printWriter = new PrintWriter(System.out);
+        } else {
+            printWriter = new PrintWriter(new File(outputFilePath));
+        }
+
         AsmifierWrapper.asmify(classFilePath, printWriter);
         return 0;
     }
